4c4e3a9f66a0cca03666631a7d1b88272ff90c76
Timothy Chandler <tschandler@gmail.com>
2020-07-12 00:54:16 +0200

    nav

--- src/app.js
+++ src/app.js
@@ -3,13 +3,32 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Home from './home';
 import Gallery from './gallery';
-import Nav from './nav';
 
 export default function App() {
+    const [infoVisible, setInfoVisible] = useState(false);
+
+    const toggleInfo = () => {
+        setInfoVisible(!infoVisible);
+    };
+
     return (
         <BrowserRouter>
             <>
-                <Nav />
+                <div className='nav'>
+                    <div onClick={toggleInfo} className='toggle'>
+                        <img alt='Information' src='images/square.svg' />
+                    </div>
+                    {infoVisible && (
+                        <div className='info'>
+                            <p>
+                                Tahmooris Ramazankhani is a photographer based
+                                in Berlin
+                            </p>
+                            <p>Email: tahmooris@gmail.com</p>
+                            <p>Website by Tim Chandler</p>
+                        </div>
+                    )}
+                </div>
                 <Route exact path='/' component={Home} />
                 <Route path='/work' component={Gallery} />
             </>
--- src/gallery.js
+++ src/gallery.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 // import axios from 'axios';
 
 export default function Gallery() {
+    const [space, setSpace] = useState(1);
+    const [spaces, setSpaces] = useState([]);
+
+    useEffect(() => {
+        setSpaces([1, 2, 3, 4, 5, 6]);
+        console.log(space);
+    }, [space]);
+
+    const clickNext = () => {
+        if (space === spaces.length) {
+            setSpace(1);
+        } else {
+            setSpace(space + 1);
+        }
+    };
+
+    const clickPrevious = () => {
+        if (space === 1) {
+            setSpace(spaces.length);
+        } else {
+            setSpace(space - 1);
+        }
+    };
+
     return (
         <div className='gallery'>
             <div className='gallery-nav'>
-                <div className='previous'></div>
-                <div className='next'></div>
+                <div onClick={clickPrevious} className='previous'></div>
+                <div onClick={clickNext} className='next'></div>
             </div>
-            <div className='first'>
-                <h1>TAHMOORIS</h1>
-            </div>
-            <div className='last'>
-                <h1>RAMAZANKHANI</h1>
-            </div>
+            {space == 1 && (
+                <div className='space one'>
+                    <div className='container'>
+                        <img src='images/s1.jpg' />
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
